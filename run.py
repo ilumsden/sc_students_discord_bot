f08dc06cda9c4869e88dc926cf79b45fbdfa97c3
ilumsden <lumsden.ian@gmail.com>
2022-11-07 11:41:43 -0500

    Adds comments

--- run.py
+++ run.py
@@ -2,20 +2,28 @@ import discord
 
 import os
 
+# Import cogs, commands, etc. from the sublib
 from sc_students_bot import *
 
+# Enable the "members" and "message_content" intents
+# Note: requires the Discord Appplication to have the
+#       correct permissions
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
+# Create the Bot
 bot = discord.Bot(intents=intents)
 
-define_command(bot, add_role_to_whitelist, pass_context=True)
-define_command(bot, remove_role_from_whitelist, pass_context=True)
-define_command(bot, view_role_whitelist, pass_context=True)
-define_command(bot, purge_role_whitelist, pass_context=True)
+# Create a MemberPurgeWhitelist to enable the "member_purge" command group
+mem_purge_cog = MemberPurgeWhitelist(bot)
 
+# Add commands to Bot
+bot.add_cog(mem_purge_cog)
+
+# Make sure a Discord token is provided via environment variable
 if "DISCORD_TOKEN" not in os.environ:
     raise ValueError("DISCORD_TOKEN not set!")
 
+# Run the Bot
 bot.run(os.environ["DISCORD_TOKEN"])
--- sc_students_bot/__init__.py
+++ sc_students_bot/__init__.py
@@ -1,4 +1,13 @@
 from .mod_commands import *
 
 def define_command(bot, command_func, **kwargs):
+    """Utility function to add a command to a bot from :code:`run.py`.
+
+    :param bot: the bot to add the command to
+    :type bot: discord.Bot
+    :param command_func: the function to define as a new command
+    :type command_func: Callable[[discord.ApplicationContext, ...], Awaitable[None]]
+    :param \**kwargs: keyword arguments to be passed to discord.Bot.command
+    :type \**kwargs: Any
+    """
     return bot.command(**kwargs)(command_func)
